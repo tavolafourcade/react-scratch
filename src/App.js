274589b861560eca16fa7641b7eaba24ce864ba2
tavolafourcade <tavolafourcade@gmail.com>
2022-07-25 10:49:45 -0500

    [ ADD ] get data using fetch

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from 'react';
+//Get data from the server
+// console.log(fetch('https://jsonplaceholder.typicode.com/users')
+// .then(res => res.json()
+// .then(data => console.log(data))))
+
+//Post data to the server
+// console.log(fetch('https://jsonplaceholder.typicode.com/users', {
+//   method: 'POST',
+//   headers: {
+//     'Content-Type': 'application/json'
+//   },
+//   body: JSON.stringify({
+//     name: 'John Doe',
+//   })
+// })
+// .then(res => {
+//   return res.json()
+//   })
+// .then(data => console.log(data))
+// )
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [data, setData] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => setData(data))
+  }, []);
+return (
+    <>
+    {data.map(user => (
+      <div key={user.id}>
+        <h1>{user.name}</h1>
+        <p>{user.email}</p>
+      </div>))}
+    </>
   );
 }
 
